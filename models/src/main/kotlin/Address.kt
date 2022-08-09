d63887abdeb043452d78f6e87ab64ce65ba6935a
jaguililla <juanjoaguililla@gmail.com>
2022-08-09 08:15:16 +0200

    Allow address without address line

--- models/src/main/kotlin/Address.kt
+++ models/src/main/kotlin/Address.kt
@@ -7,18 +7,18 @@ package com.hexagonkt.models
  * @property region First level administrative region.
  */
 data class Address(
-    val addressLine: String,
     val postalCode: String,
     val countryIso: String,
+    val addressLine: String? = null,
     val locality: String? = null,
     val subregion: String? = null,
     val region: String? = null,
 ) {
 
     init {
-        require(addressLine.isNotBlank()) { "'addressLine' cannot be blank" }
         require(postalCode.isNotBlank()) { "'postalCode' cannot be blank" }
         require(countryIso.isNotBlank()) { "'countryIso' cannot be blank" }
+        require(addressLine?.isNotBlank() ?: true) { "'addressLine' cannot be blank" }
         require(locality?.isNotBlank() ?: true) { "'locality' cannot be blank" }
         require(subregion?.isNotBlank() ?: true) { "'subregion' cannot be blank" }
         require(region?.isNotBlank() ?: true) { "'region' cannot be blank" }
