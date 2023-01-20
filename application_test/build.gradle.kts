ea2b7cc9ad32a5cf2b28f47595ac427efd881f7f
jaguililla <juanjoaguililla@gmail.com>
2023-01-20 15:00:28 +0100

    Fix build

--- application_test/build.gradle.kts
+++ application_test/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 val gradleScripts = properties["gradleScripts"]
 
 apply(from = "$gradleScripts/kotlin.gradle")
-apply(from = "$gradleScripts/detekt.gradle")
 apply(from = "$gradleScripts/application.gradle")
 apply(from = "$gradleScripts/native.gradle")
 
