9f6abe056a1e7f93a1a4b0cff38cbbe63467b639
jaguililla <juanjoaguililla@gmail.com>
2023-05-02 19:47:32 +0200

    Improve Terminal handling

--- application_test/src/main/kotlin/com/hexagonkt/application/test/Application.kt
+++ application_test/src/main/kotlin/com/hexagonkt/application/test/Application.kt
@@ -1,51 +1,34 @@
 package com.hexagonkt.application.test
 
+import com.hexagonkt.core.Ansi.CSI
 import com.hexagonkt.helpers.exec
 import com.hexagonkt.terminal.AnsiCursor
 import com.hexagonkt.terminal.AnsiScreen
+import java.lang.management.ManagementFactory.getRuntimeMXBean
 
 fun main() {
-    // ps o tty=,pid= <jvm-pid>
-    // Pass variable to program (maybe in launcher) PTS=$(tty) command
-    val pts = System.getenv("PTS") ?: "/dev/pts/0"
-    "stty raw -echo -F$pts".exec()
+    val pid = getRuntimeMXBean().pid
+    val pts = System.getenv("PTS") ?: "ps o tty= $pid".exec()
+
+    "stty raw -echo -F/dev/$pts".exec()
     print(AnsiScreen.privateMode())
-    val r = System.`in`
     print(AnsiCursor.HIDE)
+
+    print(AnsiCursor.position(999, 999))
+    print("${CSI}6n") // Print cursor position (placed at bottom-right corner)
     print(AnsiCursor.HOME)
+
+    val r = System.`in`
     while (true) {
         val c = r.read()
         if (c == 'q'.code)
             break
         else
-            println(c)
+            print("${c.toChar()} ($c)")
     }
+
     print(AnsiCursor.HOME)
     print(AnsiCursor.SHOW)
     print(AnsiScreen.clear())
-    "stty cooked echo -F$pts".exec()
-}
-
-private fun terminal() {
-    print("DEMO")
-    Thread.sleep(2_000)
-    print(AnsiScreen.privateMode())
-    Thread.sleep(2_000)
-    print(AnsiCursor.HOME)
-    print(AnsiCursor.SAVE)
-    Thread.sleep(2_000)
-    print("Start")
-    Thread.sleep(2_000)
-    print(AnsiCursor.position(5, 10))
-    Thread.sleep(2_000)
-    print("End")
-    Thread.sleep(2_000)
-    print(AnsiCursor.RESTORE)
-    Thread.sleep(2_000)
-    print("Overwrite")
-    Thread.sleep(2_000)
-    print(AnsiScreen.privateMode(false))
-    Thread.sleep(2_000)
-    print("END")
-    Thread.sleep(5_000)
+    "stty cooked echo -F/dev/$pts".exec()
 }
