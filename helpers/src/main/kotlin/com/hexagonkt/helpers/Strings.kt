54ef20308eaa09cd752d914d896049b53289484a
jaguililla <juanjoaguililla@gmail.com>
2023-06-03 13:09:08 +0200

    Move functionality to `core`

--- helpers/src/main/kotlin/com/hexagonkt/helpers/Strings.kt
+++ helpers/src/main/kotlin/com/hexagonkt/helpers/Strings.kt
@@ -5,62 +5,10 @@ import kotlin.IllegalArgumentException
 import java.text.Normalizer.Form.NFD
 import java.text.Normalizer.normalize
 
-private const val VARIABLE_PREFIX = "{{"
-private const val VARIABLE_SUFFIX = "}}"
-
 val CAMEL_CASE: Regex = Regex("[a-z]+([A-Z][a-z0-9]+)+")
 val PASCAL_CASE: Regex = Regex("([A-Z][a-z0-9]+)+")
 val SNAKE_CASE: Regex = Regex("[A-Za-z]+(_[A-Za-z0-9]+)+")
 val KEBAB_CASE: Regex = Regex("[A-Za-z]+(-[A-Za-z0-9]+)+")
-
-/**
- * Filter the target string substituting each key by its value. The keys format resembles Mustache's
- * one: `{{key}}` and all occurrences are replaced by the supplied value.
- *
- * If a variable does not have a parameter, it is left as it is.
- *
- * @param parameters The map with the list of key/value tuples.
- * @return The filtered text or the same string if no values are passed or found in the text.
- * @sample com.hexagonkt.helpers.StringsSamplesTest.filterVarsExample
- */
-fun String.filterVars(parameters: Map<*, *>): String =
-    this.filter(
-        VARIABLE_PREFIX,
-        VARIABLE_SUFFIX,
-        *parameters
-            .filterKeys { it != null }
-            .map { (k, v) -> k.toString() to v.toString() }
-            .toTypedArray()
-    )
-
-/**
- * Filter the target string substituting each key by its value. The keys format resembles Mustache's
- * one: `{{key}}` and all occurrences are replaced by the supplied value.
- *
- * If a variable does not have a parameter, it is left as it is.
- *
- * @param parameters vararg of key/value pairs.
- * @return The filtered text or the same string if no values are passed or found in the text.
- * @sample com.hexagonkt.helpers.StringsSamplesTest.filterVarsVarargExample
- *
- * @see filterVars
- */
-fun String.filterVars(vararg parameters: Pair<*, *>) =
-    this.filterVars(mapOf(*parameters))
-
-/**
- * [TODO](https://github.com/hexagonkt/hexagon/issues/271).
- *
- * @receiver .
- * @param prefix .
- * @param suffix .
- * @param parameters .
- * @return .
- */
-fun String.filter(prefix: String, suffix: String, vararg parameters: Pair<String, *>): String =
-    parameters.fold(this) { result, (first, second) ->
-        result.replace(prefix + first + suffix, second.toString())
-    }
 
 /**
  * [TODO](https://github.com/hexagonkt/hexagon/issues/271).
