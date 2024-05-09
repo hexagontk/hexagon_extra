4db6b5010cc2fc68237a10b1d077b80f5b43ae39
jaguililla <juanjoaguililla@gmail.com>
2024-05-09 20:15:47 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,8 @@ import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.ALL
  */
 
 plugins {
-    kotlin("jvm") version("1.9.23") apply(false)
+//    kotlin("jvm") version("1.9.24") apply(false)
+    kotlin("jvm") version("2.0.0-RC2") apply(false)
 
     id("idea")
     id("eclipse")
